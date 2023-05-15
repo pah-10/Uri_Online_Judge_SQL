937cd6c8926bcd22ac3954c135f6090b474c8eb9
Paola Capita <61804889+pah-10@users.noreply.github.com>
2023-05-15 17:31:40 -0300

    Update problem2606.sql

--- Consultas/problem2606.sql
+++ Consultas/problem2606.sql
@@ -1,4 +1,7 @@
---Categorias
+/*
+  2606 - Categorias
+  Quando os dados foram migrados de Banco de Dados, houve um pequeno mal-entendido por parte do antigo DBA ...
+*/
 
 SELECT prod.id, prod.name
 FROM products prod
