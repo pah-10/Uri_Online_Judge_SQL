e99f4278cd55af833e24f3be73292192f7b563c2
Paola Capita <61804889+pah-10@users.noreply.github.com>
2023-05-15 17:18:34 -0300

    Update problem2622.sql

--- Consultas/problem2622.sql
+++ Consultas/problem2622.sql
@@ -1,4 +1,7 @@
--- Pessoas jurídicas
+/*
+  2622 - Pessoas Jurídicas
+  O setor de vendas quer fazer uma promoção para todos os clientes que são pessoas jurídicas. Para isso ...
+*/
 
 SELECT cliente.name
 FROM customers as cliente
