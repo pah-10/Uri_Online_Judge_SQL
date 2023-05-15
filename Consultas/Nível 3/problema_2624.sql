ee3dadef429ac87e6dc46bde251bf64719d059d0
Paola Capita <61804889+pah-10@users.noreply.github.com>
2023-05-15 18:08:08 -0300

    Create problema_2624.sql

---
+++ Consultas/Nível 3/problema_2624.sql
@@ -0,0 +1,7 @@
+/*
+  2624 - Quantidades de Cidades por Clientes
+    A diretoria da empresa pediu para você um relatório simples de quantas cidades a empresa já alcançou ...
+*/
+
+SELECT COUNT(DISTINCT city)
+FROM customers
