f883275759b728a3e0650cf4c3cad178c1028dea
Paola Capita <61804889+pah-10@users.noreply.github.com>
2023-05-15 18:34:39 -0300

    Update and rename Consultas/problem2738.sql to Consultas/Nível 4/problema_2738.sql

--- Consultas/Nível 4/problema_2738.sql
+++ Consultas/Nível 4/problema_2738.sql
@@ -1,4 +1,7 @@
--- CONCURSO
+/*
+  2738 - Concurso
+  A Universidade Tecnológica de Marte está com seu concurso aberto para Pesquisadores. Porém o computador ...
+*/
 
 SELECT c.name, ROUND((((s.math * 2) + (s.specific * 3) + (s.project_plan * 5))/ 10), 2) AS avg
 FROM score s
